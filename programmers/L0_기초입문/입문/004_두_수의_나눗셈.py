fa2f9ed431891d376802dad7b33c877b88e76b40
shkhsxx <shkhsxx@gmail.com>
2026-02-13 09:29:50 +0900

    [프로그래머스] L0 - 두 수의 나눗셈 (#4)

---
+++ programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
@@ -0,0 +1,14 @@
+# 두 수의 나눗셈
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120806
+# 알고리즘: 기초
+# 작성자: 김혜수
+# 작성일: 2026. 02. 13. 09:29:49
+
+def solution(num1, num2):
+    answer = (num1/num2)*1000
+    print(int(answer))
+    return int(answer)
+
+
+# 나눗셈 후 나머지 값은 버리고 몫만 남겨서 X1000을 해야 할 것 같은데...
