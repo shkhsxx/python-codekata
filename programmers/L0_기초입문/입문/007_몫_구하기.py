f8dc7f54bf10f71555fbc87cc2c9225a452565bc
shkhsxx <shkhsxx@gmail.com>
2026-02-13 09:21:26 +0900

    [프로그래머스] L0 - 몫 구하기 (#7)

---
+++ programmers/L0_기초입문/입문/007_몫_구하기.py
@@ -0,0 +1,13 @@
+# 몫 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120805
+# 알고리즘: 기초
+# 작성자: 김혜수
+# 작성일: 2026. 02. 13. 09:21:25
+
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+# 예시 1은 나눗셈 후 나머지 값이 없어서 통과,
+# 예시 2는 나머지 값이 있어서 통과 안된 것 같은데...음..
