ad9b4e0fbb803f9a7185fe1113ccd9c3e098558b
shkhsxx <shkhsxx@gmail.com>
2026-02-17 18:21:23 +0900

    [프로그래머스] L0 - 두 수의 곱 구하기 (#6)

--- programmers/L0_기초입문/입문/006_두_수의_곱_구하기.py
+++ programmers/L0_기초입문/입문/006_두_수의_곱_구하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120804
 # 알고리즘: 기초
 # 작성자: 김혜수
-# 작성일: 2026. 02. 13. 02:11:50
+# 작성일: 2026. 02. 17. 18:21:22
 
 def solution(num1, num2):
     answer = num1*num2
