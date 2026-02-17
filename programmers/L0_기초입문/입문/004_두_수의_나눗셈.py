d2c17b709660251846bdd6cee0d6bdd7dcd5f64a
shkhsxx <shkhsxx@gmail.com>
2026-02-17 18:23:06 +0900

    [프로그래머스] L0 - 두 수의 나눗셈 (#4)

--- programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
+++ programmers/L0_기초입문/입문/004_두_수의_나눗셈.py
@@ -3,11 +3,11 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120806
 # 알고리즘: 기초
 # 작성자: 김혜수
-# 작성일: 2026. 02. 17. 18:18:26
+# 작성일: 2026. 02. 17. 18:23:06
 
 def solution(num1, num2):
     answer = (num1/num2)*1000
     return int(answer)
 
 
-# 나눗셈 후 나머지 값은 버리고 몫만 남겨서 X1000을 해야 할 것 같은데...
+# 나눈 값을 정수로 return해야 하니까 int() 형변환 필수
